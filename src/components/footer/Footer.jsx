ad4561b8925f213fea836dffcce1a226ac9cc3c7
ahosanhabib2121 <habibmia21021@gmail.com>
2023-12-11 11:18:30 +0600

    footer section added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,11 @@
 import { Link } from "react-router-dom";
 import Container from "../container/Container";
+import { MdCall, MdEmail } from "react-icons/md";
+import { GrFacebookOption } from "react-icons/gr";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import logo from '../../assets/image/logo.png'
+import { FaAddressCard } from "react-icons/fa6";
+import moment from 'moment';
 
 const Footer = () => {
     return (
@@ -8,78 +14,61 @@ const Footer = () => {
                 <Container>
                     <footer className="footer justify-center md:px-5 md:justify-normal lg:px-0 py-20  font-fontRoboto ">
                         <aside>
-                            <div className=" flex  md:gap-2 items-center">
+                            <div className=" flex  md:gap-1 items-center">
                                 <img
-                                    // src={logo}
+                                    src={logo}
                                     alt="not found"
                                     className=" w-20 md:w-24 h-auto"
                                 />
                                 <Link
                                     to="/"
-                                    className=" uppercase text-lg md:text-2xl font-semibold tracking-wider font-robotFont"
+                                    className=" uppercase text-lg md:text-2xl font-bold tracking-wider text-[#55E6A5]"
                                 >
                                     Portfolio
                                 </Link>
                             </div>
-                            <p className=" text-sm text-[#94A3B8]">
-                                Portfolio Etiam interdum arcu metus, <br /> eget ultricies eros euismod ut.
+                            <p className=" font-medium text-base text-[#a2a2a2]">
+                                This website private is made for me,<br /> where I will have all the information.
                             </p>
-                            {/* <div className=" flex gap-2 text-[#94A3B8]">
-                                <MdEmail className=" text-xl" />
-                                contact@yourdomain.com
-                            </div>
-                            <div className=" flex gap-2 text-[#94A3B8]">
-                                <MdCall className=" text-xl" />
-                                Phone: +1(2)345 6789
-                            </div> */}
+                            
 
                         </aside>
                         <nav>
                             <header className=" footer-title text-xl mb-4 text-white  opacity-100 ">
-                                Services
+                                Contact Info
                             </header>
-                            <div className=" space-y-3 text-[#94A3B8]">
-                                <div className=" flex gap-2">
-                                    <Link className="link link-hover hover:text-[#b675f6ec]">
-                                        E-Learning
-                                    </Link>
+                            <div className=" space-y-3 text-[#a2a2a2]">
+                                <div className=" flex gap-2 text-[#a2a2a2]">
+                                    <MdEmail className=" text-xl" />
+                                    habibmia21021@gmail.com
                                 </div>
-                                <div className=" flex gap-2">
-                                    <Link className="link link-hover hover:text-[#b675f6ec]">
-                                        Bootcamp
-                                    </Link>
+                                <div className=" flex gap-2 text-[#a2a2a2]">
+                                    <MdCall className=" text-xl" />
+                                    Phone: +8801969440721
                                 </div>
-                                <div className=" flex gap-2">
-                                    <Link className="link link-hover hover:text-[#b675f6ec]">
-                                        Webinar
-                                    </Link>
+                                <div className=" flex gap-2 text-[#a2a2a2]">
+                                    <FaAddressCard className=" text-xl" />
+                                    Comilla, Dhaka, Bangladesh
                                 </div>
                             </div>
                         </nav>
                         <nav>
                             <header className="footer-title text-xl mb-4 text-white  opacity-100">
-                                Company
+                                Social Icon
                             </header>
-                            <Link className="link link-hover text-[#94A3B8] hover:text-[#b675f6ec]">
-                                Home
-                            </Link>
-                            <Link className="link link-hover text-[#94A3B8] hover:text-[#b675f6ec]">
-                                All Classes
-                            </Link>
-                        </nav>
-                        <nav>
-                            <header className="footer-title text-xl mb-4 text-white  opacity-100">
-                                Newsletter
-                            </header>
-                            {/* <div>
-                                <p className=" max-w-xs mb-4 text-[#94A3B8]">Sign up our newsletter to get update information, news and free insight.</p>
-                                <Link to='/signUp'>
-                                    <Button
-                                        label={'Sign up'}
-                                    />
+                             <div className="flex gap-4">
+                                <Link to="https://www.facebook.com/" target="_blank" className="bg-[#504f4f] p-2 rounded-full hover:bg-[#55E6A5] ">
+                                    <GrFacebookOption className="text-2xl " />
                                 </Link>
-                            </div> */}
+                                <Link to="https://www.linkedin.com/in/habib2121/" target="_blank" className="bg-[#504f4f] p-2 rounded-full hover:bg-[#55E6A5] ">
+                                    <FaLinkedinIn className="text-2xl " />
+                                </Link>
 
+                                <Link to="https://github.com/HabibMia2121" target="_blank" className="bg-[#504f4f] p-2 rounded-full hover:bg-[#55E6A5] ">
+                                    <FaGithub className="text-2xl " />
+                                </Link>
+                                
+                            </div>
                         </nav>
                     </footer>
                 </Container>
@@ -87,25 +76,9 @@ const Footer = () => {
             {/* copyright section here  */}
             <div className="bg-[#080D13] text-white">
                 <Container>
-                    <div className="flex flex-col-reverse items-center gap-4 lg:flex-row lg:text-start lg:justify-between text-center  py-9">
-                        <div className="grid justify-center lg:justify-start">
-                            <div className="flex gap-4">
-                                {/* <a href="#" className="bg-[#504f4f] p-2 rounded-full hover:bg-[#8d55c5ec] ">
-                                    <GrFacebookOption className="text-2xl " />
-                                </a>
-                                <a href="#" className="bg-[#504f4f] p-2 rounded-full hover:bg-[#8d55c5ec] ">
-                                    <IoLogoInstagram className="text-2xl " />
-                                </a>
-                                <a href="#" className="bg-[#504f4f] p-2 rounded-full hover:bg-[#8d55c5ec] ">
-                                    <IoLogoTwitter className="text-2xl " />
-                                </a>
-                                <a href="#" className="bg-[#504f4f] p-2 rounded-full hover:bg-[#8d55c5ec] ">
-                                    <FaLinkedinIn className="text-2xl " />
-                                </a> */}
-                            </div>
-                        </div>
-                        <span className=" text-[#94A3B8] ">
-                            Copyright © 2020. All rights reserved by, Portfolio
+                    <div className=" text-center  py-9">
+                        <span className=" text-[#a2a2a2] ">
+                            Copyright © {moment().format('YYYY')}. All rights reserved by, Portfolio
                         </span>
                     </div>
                 </Container>
